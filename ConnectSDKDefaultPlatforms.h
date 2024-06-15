73ee5cb687b3553f8fa5fb79e7f1d132a0016e86
Dr0gis <dr0gissf@gmail.com>
2024-06-15 11:06:25 -0700

    TV-113 add new android service

--- ConnectSDKDefaultPlatforms.h
+++ ConnectSDKDefaultPlatforms.h
@@ -22,6 +22,7 @@
 
 #define kConnectSDKDefaultPlatforms (@{ \
     @"AirPlayService"     : @"ZeroConfDiscoveryProvider", \
+    @"NewAndroidService"  : @"ZeroConfDiscoveryProvider", \
     @"DIALService"        : @"SSDPDiscoveryProvider", \
     @"DLNAService"        : @"SSDPDiscoveryProvider", \
     @"NetcastTVService"   : @"SSDPDiscoveryProvider", \
